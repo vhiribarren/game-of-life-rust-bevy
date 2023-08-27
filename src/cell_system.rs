a84bfd4d57379d7861cd32fdef441697ebf56a4e
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-08-27 09:59:28 +0200

    Refactor cell system in putting it in own file and bevy plugin

---
+++ src/cell_system.rs
@@ -0,0 +1,105 @@
+/*
+MIT License
+
+Copyright (c) 2023 Vincent Hiribarren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+use std::collections::BTreeSet;
+
+use bevy::{prelude::*, utils::HashMap};
+
+static NEIGHBOURS_DELTA: [(isize, isize); 8] = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+];
+
+#[derive(Clone, Component, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
+struct Position {
+    x: isize,
+    y: isize,
+}
+
+pub struct CellSystem;
+
+impl Plugin for CellSystem {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, init_cells)
+            .add_systems(Update, system_cells);
+    }
+}
+
+fn init_cells(mut commands: Commands) {
+    commands.spawn(Position { x: 0, y: 0 });
+    commands.spawn(Position { x: 1, y: 0 });
+    commands.spawn(Position { x: -1, y: 0 });
+    commands.spawn(Position { x: 0, y: 1 });
+}
+
+fn system_cells(mut commands: Commands, query: Query<(Entity, &Position)>) {
+    let mut neighbours = HashMap::new();
+    let mut spawn_candidates = BTreeSet::new();
+    // Compute number of alive neighbour cells
+    for (_, cell) in &query {
+        for pos_delta in NEIGHBOURS_DELTA.iter() {
+            let scan_pos = Position {
+                x: cell.x + pos_delta.0,
+                y: cell.y + pos_delta.1,
+            };
+            let neighbours_count = match neighbours.get(&scan_pos) {
+                Some(prev_val) => prev_val + 1,
+                None => 1,
+            };
+            neighbours.insert(scan_pos.clone(), neighbours_count);
+            if neighbours_count == 3 {
+                // This cell has 3 neighours, might spawn a new cell
+                spawn_candidates.insert(scan_pos.clone());
+            } else if neighbours_count == 4 {
+                // Actually, this cell has too many neighbours, removing the candidate
+                spawn_candidates.remove(&scan_pos);
+            }
+        }
+    }
+    // Killing starved or overpopulated cells
+    for (entity, cell) in &query {
+        let neighbours_count = *neighbours
+            .get(cell)
+            .expect("Shoud have been inserted in previous loop");
+        println!("cell {:?} has {:?} neighbours", cell, neighbours.get(cell));
+        match neighbours_count {
+            0..=1 => commands.entity(entity).despawn(),
+            2 => (),
+            3 => {
+                spawn_candidates.remove(cell);
+            }
+            _ => commands.entity(entity).despawn(),
+        }
+    }
+    // Spawn new cells
+    for new_cell in spawn_candidates {
+        println!("spawn new cell {:?}", new_cell);
+        commands.spawn(new_cell);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-use std::collections::BTreeSet;
 
-use bevy::{prelude::*, utils::HashMap};
+mod cell_system;
+
+use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use cell_system::CellSystem;
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.3, 0.6);
 
@@ -33,10 +35,9 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
+        .add_plugins(CellSystem)
         .add_systems(Startup, init_camera)
-        .add_systems(Startup, init_cells)
         .add_systems(Update, system_gui)
-        .add_systems(Update, system_cells)
         .run();
 }
 
@@ -69,74 +70,4 @@ fn system_gui(mut contexts: EguiContexts) {
             ui.add(egui::Separator::default());
             ui.add(egui::Slider::new(&mut 100, 0..=100).integer().text("Zoom"));
         });
-}
-
-fn init_cells(mut commands: Commands) {
-    commands.spawn(Position { x: 0, y: 0 });
-    commands.spawn(Position { x: 1, y: 0 });
-    commands.spawn(Position { x: -1, y: 0 });
-    commands.spawn(Position { x: 0, y: 1 });
-}
-
-static NEIGHBOURS_DELTA: [(isize, isize); 8] = [
-    (-1, -1),
-    (0, -1),
-    (1, -1),
-    (-1, 0),
-    (1, 0),
-    (-1, 1),
-    (0, 1),
-    (1, 1),
-];
-
-fn system_cells(mut commands: Commands, query: Query<(Entity, &Position)>) {
-    let mut neighbours = HashMap::new();
-    let mut spawn_candidates = BTreeSet::new();
-    // Compute number of alive neighbour cells
-    for (_, cell) in &query {
-        for pos_delta in NEIGHBOURS_DELTA.iter() {
-            let scan_pos = Position {
-                x: cell.x + pos_delta.0,
-                y: cell.y + pos_delta.1,
-            };
-            let neighbours_count = match neighbours.get(&scan_pos) {
-                Some(prev_val) => prev_val + 1,
-                None => 1,
-            };
-            neighbours.insert(scan_pos.clone(), neighbours_count);
-            if neighbours_count == 3 {
-                // This cell has 3 neighours, might spawn a new cell
-                spawn_candidates.insert(scan_pos.clone());
-            } else if neighbours_count == 4 {
-                // Actually, this cell has too many neighbours, removing the candidate
-                spawn_candidates.remove(&scan_pos);
-            }
-        }
-    }
-    // Killing starved or overpopulated cells
-    for (entity, cell) in &query {
-        let neighbours_count = *neighbours
-            .get(cell)
-            .expect("Shoud have been inserted in previous loop");
-        println!("cell {:?} has {:?} neighbours", cell, neighbours.get(cell));
-        match neighbours_count {
-            0..=1 => commands.entity(entity).despawn(),
-            2 => (),
-            3 => {
-                spawn_candidates.remove(cell);
-            }
-            _ => commands.entity(entity).despawn(),
-        }
-    }
-    // Spawn new cells
-    for new_cell in spawn_candidates {
-        println!("spawn new cell {:?}", new_cell);
-        commands.spawn(new_cell);
-    }
-}
-
-#[derive(Clone, Component, PartialEq, Eq, Debug, Hash, PartialOrd, Ord)]
-struct Position {
-    x: isize,
-    y: isize,
 }
